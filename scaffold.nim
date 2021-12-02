12d4324e26939ef664184fcd3f9e8e78483707d1
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2021-12-02 06:38:38 +0100

    update scaffold

--- scaffold.nim
+++ scaffold.nim
@@ -5,14 +5,18 @@ let baseName = paramStr(1)
 let mainNimContent = """
 import std/strutils
 
-func getResult(input: seq[string]): int =
+func getResultPart1(input: seq[string]): int =
+    0
+
+func getResultPart2(input: seq[string]): int =
     0
 
 when isMainModule:
     let input = readFile("input.txt")
     let lines = splitLines(input)
 
-    echo getResult(lines)
+    echo "part1 result:" & getResultPart1(lines)
+    echo "part2 result:" & getResultPart2(lines)
 """
 
 createDir(joinPath(baseName,"src"))
